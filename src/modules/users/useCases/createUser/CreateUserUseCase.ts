9aad9aba7443c11a659aa8523a7257584a4694b5
Daniel Moura <daniel-moura@outlook.com>
2023-01-22 23:03:23 -0300

    feat: implement create user use case

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
-import { User } from "../../model/User";
+import { User } from "modules/users/model/User";
+
+import { AppError } from "../../error/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -10,7 +12,20 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const user = {
+      name,
+      email,
+    };
+
+    const userAlreadyExist = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExist) {
+      throw new AppError("User already exist");
+    }
+
+    const createdUser = this.usersRepository.create(user);
+
+    return createdUser;
   }
 }
 
