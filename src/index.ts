03108ae2f50e8672389452780ca0afb0c62c0b26
Daniel Moura <daniel-moura@outlook.com>
2023-01-23 02:17:09 -0300

    feat: add swagger ui express and config app to use swagger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { NextFunction, Request, Response } from "express";
+import sweaggerUi from "swagger-ui-express";
 
 import "express-async-errors";
 
 import { AppError } from "./modules/users/error/AppError";
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", sweaggerUi.serve, sweaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
