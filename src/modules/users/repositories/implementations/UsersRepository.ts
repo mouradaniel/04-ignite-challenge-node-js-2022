20319726cbb196107f9fe3a561136e8d1cd58e7f
Daniel Moura <daniel-moura@outlook.com>
2023-01-22 23:02:37 -0300

    feat: implement create, find by id, find by email, turn admin and list on repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,39 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      admin: false,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user_to_turn_admin = this.users.find(
+      (user) => user.id === receivedUser.id
+    );
+    user_to_turn_admin.admin = true;
+    return user_to_turn_admin;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
