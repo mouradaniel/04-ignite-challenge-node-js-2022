bac70323ae59d158b875c96ac7df7dfef38d3871
Daniel Moura <daniel-moura@outlook.com>
2023-01-23 01:39:02 -0300

    feat: implement turn user admin use case

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -1,15 +1,23 @@
+import { AppError } from "../../error/AppError";
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[] | undefined;
 }
 
 class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User doesn't exist", 404);
+    }
+
+    const admin_user = this.usersRepository.turnAdmin(user);
+    return admin_user;
   }
 }
 
