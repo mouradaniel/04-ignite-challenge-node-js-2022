c54510f996a9f4aaa76f4c5519de10da7077a3fe
Daniel Moura <daniel-moura@outlook.com>
2023-01-23 01:37:44 -0300

    feat: implement list all users use case

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,28 @@
+import { AppError } from "../../error/AppError";
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id?: string | string[] | undefined;
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exist");
+    }
+
+    if (!user.admin) {
+      throw new AppError("User doesn't have permission");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
